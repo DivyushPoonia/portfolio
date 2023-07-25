aeb59ecf3af74631e2f58d81da3e303d30093a05
DivyushPoonia <77872393+DivyushPoonia@users.noreply.github.com>
2023-07-25 22:12:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+
+app.listen('3000',()=>{
+    console.log('listening to port 3000');
+})
+
+
+
+app.get('/', (req, res)=>{
+    res.render('main.ejs')
+})
